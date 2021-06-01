6a68d851c8e481b57706ee7f9f3ffbb469d85b06
AnbuStyle <khumaun99@gmail.com>
2021-06-01 21:31:24 +0300

    update

--- src/main/java/ru/geekbrains/efremov/HomeWork.java
+++ src/main/java/ru/geekbrains/efremov/HomeWork.java
@@ -2,6 +2,12 @@ package ru.geekbrains.efremov;
 
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("Привет");
+
+        System.out.println();
+    }
+    public static void insert () {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
