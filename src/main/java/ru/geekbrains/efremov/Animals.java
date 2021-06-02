556717c7d44b549e390f0eef901773aedc2cb6f7
AnbuStyle <khumaun99@gmail.com>
2021-06-03 02:35:10 +0300

    final home work 6

--- src/main/java/ru/geekbrains/efremov/Animals.java
+++ src/main/java/ru/geekbrains/efremov/Animals.java
@@ -1,7 +1,29 @@
 package ru.geekbrains.efremov;
 
-public class Animals {
-    public static void main(String[] args) {
+public abstract class Animals {
+    protected String name;
+    protected double maxLengthRun;
+    protected double maxLengthSwim;
+    //protected static int count;  //доработать count не успел
 
+    public Animals(String name, double maxLengthRun, double maxLengthSwim) {
+        //count++;
+        this.name = name;
+        this.maxLengthRun = maxLengthRun;
+        this.maxLengthSwim = maxLengthSwim;
+    }
+
+    //public static void checkAnimals (){
+    //  System.out.println(" Всего животных" + Animals.count);
+    //}
+
+    public void infoAnimals() {
+       //  System.out.printf("name =%s, maxLengthRun =%f, maxLengthSwim = %f", name, maxLengthRun, maxLengthSwim);
     }
 }
+
+
+
+
+
+
--- src/main/java/ru/geekbrains/efremov/Cat.java
+++ src/main/java/ru/geekbrains/efremov/Cat.java
@@ -1,4 +1,23 @@
 package ru.geekbrains.efremov;
 
-public class Cat {
+public class Cat extends Animals{
+    public Cat(String name, double maxLengthRun, double maxLengthSwim) {
+        super(name, maxLengthRun, maxLengthSwim);
+        //this.count;
+    }
+
+    protected void swimming() {
+        System.out.println("Cats can't swimming");
+    }
+
+    protected void running(double distance) {
+
+        if (maxLengthRun >= distance)
+            System.out.println(name + " is running " + distance + ".");
+        else
+            System.out.println(name + " cannot run. Max distance is " + maxLengthRun + " meters");
+    }
+
 }
+
+
